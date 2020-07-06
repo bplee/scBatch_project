766a40116e34c6841e0838c0f5602350162578d4
Brennan Lee <brennanpl2000@gmail.com>
2020-07-06 12:56:30 -0400

    small changes

--- scratch.py
+++ scratch.py
@@ -14,4 +14,9 @@ import torch.utils.data as data_utils
 from DIVA.model.model_diva import DIVA
 from DIVA.dataset.rcc_loader_semi_sup import RccDatasetSemi
 
+print("trying to use the class")
 data_obj = RccDatasetSemi(test_patient=1, x_dim=200)
+
+print("done")
+print(type(data_obt))
+
