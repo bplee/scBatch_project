4b02b703a78d26479c6bde1e34fa6aad3aebd54a
Brennan Lee <brennanpl2000@gmail.com>
2020-08-10 13:39:27 -0400

    fixing saving model filepath

--- ForBrennan/STARSPACE/run_starspace.py
+++ ForBrennan/STARSPACE/run_starspace.py
@@ -214,14 +214,15 @@ if __name__ == "__main__":
     print('Training time of StarSpace: {} mins'.format((time.time() - t0)/60)) 
 
     # Save the model:
-    sp.saveModel('starspace_model_'+str(args_starspace.test_patient))
-    sp.saveModelTsv('starspace_model_'+str(args_starspace.test_patient)+'.tsv')
+    # added in the prior folder path, since were operating out of bplee/scBatch/ccRCC
+    sp.saveModel('../starspace_models/starspace_model_'+str(args_starspace.test_patient))
+    sp.saveModelTsv('../starspace_models/starspace_model_'+str(args_starspace.test_patient)+'.tsv')
 
     # Reload the model
-    sp.initFromSavedModel('starspace_model_'+str(args_starspace.test_patient))
-    sp.initFromTsv('starspace_model_'+str(args_starspace.test_patient)+'.tsv')
+    sp.initFromSavedModel('../starspace_models/starspace_model_'+str(args_starspace.test_patient))
+    sp.initFromTsv('../starspace_models/starspace_model_'+str(args_starspace.test_patient)+'.tsv')
 
-    # Test the model and creat the file './predictfile_starspace.txt'
+    # Test the model and create the file './predictfile_starspace.txt'
     sp.evaluate()
     #sp.nearestNeighbor()
 
