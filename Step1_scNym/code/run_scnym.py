eb5f7e3b091195806a73f86c72a672efb956b9d2
Brennan Lee <brennanpl2000@gmail.com>
2020-08-27 17:30:43 -0400

    adusting for new rccloader semi sup script

--- Step1_scNym/code/run_scnym.py
+++ Step1_scNym/code/run_scnym.py
@@ -35,25 +35,26 @@ TEST_PATIENT = 5
 X_DIM = 784
 
 # getting training and testing data
-train = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=True)
-test = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=False)
+train = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=True, diva=False)
+test = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=False, diva=False)
 
+# dont need this anymore
 # reshaping it from its weird diva structure
-train_cell_num = train.train_data.shape[0]
-test_cell_num = test.test_data.shape[0]
+#train_cell_num = train.train_data.shape[0]
+#test_cell_num = test.test_data.shape[0]
 
 # need to select one patient to use as training domain:
-TRAIN_PATIENT = 3 #choose {0,...,5}i
+TRAIN_PATIENT = 3 #choose {0,...,5}
 
 # selecting all of the indices that mark our patient
 train_patient_inds = train.train_domain[:,TRAIN_PATIENT] == 1 
 # using inds to select data for our patient
-train_patient_data = train.train_data.reshape(train_cell_num, X_DIM)[train_patient_inds]
+train_patient_data = train.train_data[train_patient_inds]
 
 # making the data obj for our training and test patient
 train_adata = anndata.AnnData(np.array(train_patient_data))
-#train_adata = anndata.AnnData(np.array(train.train_data.reshape(train_cell_num, X_DIM)))
-test_adata = anndata.AnnData(np.array(test.test_data.reshape(test_cell_num, X_DIM)))
+# train_adata = anndata.AnnData(np.array(train.train_data.reshape(train_cell_num, X_DIM)))
+test_adata = anndata.AnnData(np.array(test.test_data))
 
 # converting 1 hot vectors into int labels
 train_int_labels = np.array(train.train_labels).dot(np.arange(len(train.train_labels[0]), dtype=float))
