761b7bc0b8020445ff9a729e426302eb320e2361
Brennan Lee <brennanpl2000@gmail.com>
2020-08-31 11:59:11 -0400

    changing label stuff

--- Step1_scNym/code/run_scnym.py
+++ Step1_scNym/code/run_scnym.py
@@ -39,6 +39,7 @@ X_DIM = 10000# 784 is the magic number for DIVA
 train = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=True, diva=False)
 test = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=False, diva=False)
 
+cell_types = train.cell_types
 
 # need to select one patient to use as training domain:
 TRAIN_PATIENT = 3 #choose {0,...,5}
@@ -54,12 +55,20 @@ train_adata = anndata.AnnData(np.array(train_patient_data))
 test_adata = anndata.AnnData(np.array(test.test_data))
 
 # converting 1 hot vectors into int labels
-train_int_labels = np.array(train.train_labels).dot(np.arange(len(train.train_labels[0]), dtype=float))
+train_int_labels = np.array(train.train_labels).dot(np.arange(len(train.train_labels[0]), dtype=int))
+test_int_labels = np.array(test.test_labels.dot(np.arage(len(test.test_labels[0]), dtype=int)))
 
-# setting labels by grabbing the labels for our training partient
-train_adata.obs['annotations'] = train_int_labels[train_patient_inds]
+# creating vectors of cell labels:
+train_cell_types = train.cell_types[train_int_labels]
+test_cell_types = test.cell_types[test_int_labels]
+
+# setting gold labels: (using names and not indices)
+train_adata.obs['cell_type'] = train_cell_types
+test_adata.obs['cell_type'] = test_cell_types
+
+# setting the semi_supervised labels:
+train_adata.obs['annotations'] = train_cell_types
 test_adata.obs['annotations'] = 'Unlabeled'
-
 
 print('hey')
 print(type(train_int_labels))
