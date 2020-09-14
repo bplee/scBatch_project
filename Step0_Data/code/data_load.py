af409a171d19a9f8f8de7f89286c2281330efdfd
Brennan Lee <brennanpl2000@gmail.com>
2020-09-14 10:52:07 -0400

    final changes to data load script

--- Step0_Data/code/data_load.py
+++ Step0_Data/code/data_load.py
@@ -1,20 +1,21 @@
-import os
-import sys
-
-import argparse
-import time
-import numpy as np
+from scnym.api import scnym_api
 import torch
-
-# importing scnym dependencies
-import scnym
-import scanpy as sc
+import os
+import numpy as np
 import anndata
-import json
-import urllib
-import seaborn as sns
-import matplotlib.pyplot as plt
+import sys
+import scanpy as sc
+import pandas as pd
+import scnym
 
+import urllib
+import json
+
+# allow tensorboard outputs even though TF2 is installed
+# broke the tensorboard/pytorch API
+import tensorflow as tf
+import tensorboard as tb
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 
 # changing directory to project dir
 print("________CHANGING WORKING DIR________")
@@ -23,21 +24,59 @@ WORKING_DIR = "/data/leslie/bplee/scBatch"
 os.chdir(WORKING_DIR)
 print("\tNew working dir: %s\n" % (os.getcwd()))
 
-# adding the project dir to the path to import relevant modules
+# adding the project dir to the path to import relevant modules below
 print("________CHANGING PATH_________")
 sys.path.append(WORKING_DIR)
 print("\tWorking dir appended to Sys path.")
 
-# importing a data class
-from ForBrennan.DIVA.model.model_diva import DIVA
 from ForBrennan.DIVA.dataset.rcc_loader_semi_sup import RccDatasetSemi
 
-print("trying to use the class")
-#X_DIM = 784
-X_DIM = 1200
-data_obj = RccDatasetSemi(test_patient=1, x_dim=1200, train=True, diva=False)
+# getting training and testing data
+TEST_PATIENT = 4
+X_DIM = 16323# 784 is the magic number for DIVA; 16323 is the max
 
-print("done")
-#Data is now stored in data_obj.train_data etc
+# getting training and testing data
+data_obj = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=True, test=True, diva=False)
 
+cell_types = data_obj.cell_types
+
+# making the data obj for our training and test patient
+train_adata = anndata.AnnData(np.array(data_obj.train_data))
+# train_adata = anndata.AnnData(np.array(train.train_data.reshape(train_cell_num, X_DIM)))
+test_adata = anndata.AnnData(np.array(data_obj.test_data))
+
+# converting 1 hot vectors into int labels
+train_labels_int = np.array(data_obj.train_labels).dot(np.arange(len(data_obj.train_labels[0]), dtype=int))
+test_labels_int = np.array(data_obj.test_labels).dot(np.arange(len(data_obj.test_labels[0]), dtype=int))
+
+# creating vectors of cell labels (strings):
+train_cell_types = cell_types[train_labels_int]
+test_cell_types = cell_types[test_labels_int]
+
+# setting gold labels: (using names and not indices)
+train_adata.obs['cell_type'] = train_cell_types # all training patients included
+test_adata.obs['cell_type'] = test_cell_types
+
+# setting the semi_supervised labels:
+train_adata.obs['annotations'] = train_cell_types
+test_adata.obs['annotations'] = 'Unlabeled'
+
+print('hey')
+# print(train_int_labels.dtype)
+
+# concatenating data
+adata = train_adata.concatenate(test_adata)
+
+# debugging lines:
+print(adata.obs)
+
+blurb = """
+Loaded 3 useful annData objects:
+        - adata (all data loaded, all pateints)
+        - train_adata (train patient)
+        - test_adata (test patient)
+Loaded 1 useful class that holds all data:
+        - data_obj (Rcc Semi super loader, all data)
+"""
+print(blurb)
 
