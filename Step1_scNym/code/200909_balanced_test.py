368662a4017479c0de848b2b63c856395bb42a3b
Brennan Lee <brennanpl2000@gmail.com>
2020-09-21 17:28:13 -0400

    bug fix

--- Step1_scNym/code/200909_balanced_test.py
+++ Step1_scNym/code/200909_balanced_test.py
@@ -34,6 +34,20 @@ from ForBrennan.DIVA.dataset.rcc_loader_semi_sup import RccDatasetSemi
 
 def get_balanced_classes(adata):
     """
+    Takes the n = |smallest size class of adata|, and draws random groups of size n for each class
+    and returns the random groups
+
+    Parameters
+    ----------
+    adata: anndata.Anndata obj
+        anndata obj with entries in adata.obs['cell_type']
+
+    Returns
+    -------
+    anndata.Anndata obj
+        Smaller anndata obj with balanced class distribution
+    """
+    """
     Returns an anndata obj with balanced labels
 
     Input:
@@ -56,70 +70,74 @@ def get_balanced_classes(adata):
     return adata[rtn, :]
 
 if __name__ == "__main__":
+    from .quick_prescnym_data_load import get_Rcc_adata
 
     # getting training and testing data
-    TEST_PATIENT = 4
+    TEST_PATIENT = 2
+    TRAIN_PATIENT = 4
     X_DIM = 10000# 784 is the magic number for DIVA
 
-    # getting training and testing data
-    train = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=True, diva=False)
-    test = RccDatasetSemi(test_patient=TEST_PATIENT, x_dim=X_DIM, train=False, diva=False)
+    adata, data_obj = get_Rcc_adata(
+        test_patient=TEST_PATIENT,
+        x_dim=X_DIM
+    )
 
-    cell_types = train.cell_types
-
-    # need to select one patient to use as training domain:
-    TRAIN_PATIENT = 3 #choose {0,...,5}
-
-    # selecting all of the indices that mark our patient
-    train_patient_inds = train.train_domain[:,TRAIN_PATIENT] == 1
-    # using inds to select data for our patient
-    train_patient_data = train.train_data[train_patient_inds] #doing log transformation here
-
-    # making the data obj for our training and test patient
-    train_adata = anndata.AnnData(np.array(train_patient_data))
-    # train_adata = anndata.AnnData(np.array(train.train_data.reshape(train_cell_num, X_DIM)))
-    test_adata = anndata.AnnData(np.array(test.test_data))
-
-    # converting 1 hot vectors into int labels
-    train_int_labels = np.array(train.train_labels).dot(np.arange(len(train.train_labels[0]), dtype=int))
-    test_int_labels = np.array(test.test_labels).dot(np.arange(len(test.test_labels[0]), dtype=int))
-
-    # creating vectors of cell labels:
-    train_cell_types = train.cell_types[train_int_labels]
-    test_cell_types = test.cell_types[test_int_labels]
-
-    # setting gold labels: (using names and not indices)
-    train_adata.obs['cell_type'] = train_cell_types[train_patient_inds] # there are cell types for multiple patients so we index for the one we care about
-    test_adata.obs['cell_type'] = test_cell_types
-
-    # setting the semi_supervised labels:
-    train_adata.obs['annotations'] = train_cell_types[train_patient_inds]
-    test_adata.obs['annotations'] = 'Unlabeled'
-
-    print('hey')
-    print(type(train_int_labels))
-    print(train_int_labels.dtype)
-
-    # concatenating data
-    adata = train_adata.concatenate(test_adata)
-
-    # debugging lines:
-    print(adata.obs)
-
-
-    blurb = """
-    Loaded 3 useful annData objects:
-        - adata (ready as input to scnym)
-        - train_adata
-        - test_adata
-    Ready to train scnym
-    """
-    print(blurb)
+    # blurb = """
+    # Loaded 3 useful annData objects:
+    #     - adata (ready as input to scnym)
+    #     - train_adata
+    #     - test_adata
+    # Ready to train scnym
+    # """
+    # print(blurb)
 
     # now we want to create a balanced train/testing set to check the data
 
+    patients = data_obj.patients
 
-    balanced_adata = get_balanced_classes(test_adata)
+    by_pat = [adata[adata.obs.patient==p] for p in patients]
+    all_types_by_pat = pd.DataFrame([p.obs.cell_type.value_counts() for p in by_pat]).T
+
+    all_types_by_pat > 1000
+
+    easy_cell_list = ['CD8 Exhausted_A', 'Naive CD8/CD4', 'Treg']
+
+    test_adata = adata[adata.obs.patient==patients[TEST_PATIENT]]
+    train_adata = adata[adata.obs.patient==patients[TRAIN_PATIENT]]
+
+    train_adata = train_adata[train_adata.obs.cell_type.isin(easy_cell_list)]
+    test_adata = test_adata[test_adata.obs.cell_type.isin(easy_cell_list)]
+
+    test_adata.obs.annotations = 'Unlabeled'
+
+    exp_adata = train_adata.concatenate(test_adata)
+
+    scnym_api(adata=exp_adata,
+              task='train',
+              config='no_new_identity',
+              out_path='./scnym_mini_exp_output',  # this is going in WORKING DIR
+              groupby='annotations')
+    print("Done training. Now for prediction")
+    scnym_api(
+        adata=exp_adata,
+        task='predict',
+        key_added='scNym',
+        config='no_new_identity',
+        trained_model='./scnym_mini_exp_output'
+    )
+
+    sc.pp.neighbors(exp_adata, use_rep='X_scnym', n_neighbors=30)
+    sc.tl.umap(exp_adata, min_dist=.3)
+    # the following are the scnym internal embeddings colored by batch and cell type
+    sc.pl.umap(exp_adata, color='patient', size=5, alpha=.2, save='_scnym_mini_exp_embedding_patient.png')
+    sc.pl.umap(exp_adata, color='cell_type', size=5, alpha=.2, save='_scnym_mini_exp_embedding_celltype.png')
+
+    sc.pp.neighbors(exp_adata, use_rep='X', n_neighbors=30)
+    sc.tl.umap(exp_adata, min_dist=.3)
+    sc.pl.umap(exp_adata, color='cell_type', size=5, alpha=.2, save='_scnym_mini_exp_og_data_umap_celltype.png')
+    sc.pl.umap(exp_adata, color='patient', size=5, alpha=.2, save='_scnym_mini_exp_og_data_umap_patient.png')
+
+    balanced_test_adata = get_balanced_classes(test_adata)
     print("created balanced_adata from test_adata")
 
     get_balanced_classes(train_adata)
