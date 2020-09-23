a57ef785dbc4b3926b165b1c76d903cbf2fdc254
Brennan Lee <brennanpl2000@gmail.com>
2020-09-22 23:47:10 -0400

    bug fixes

--- Step0_Data/code/new_data_load.py
+++ Step0_Data/code/new_data_load.py
@@ -9,6 +9,7 @@ readRDS = robjects.r['readRDS']
 if __name__ == "__main__":
 
     new_annot_file_path = '/data/leslie/krc3004/RCC_Alireza_Sep2020/ccRCC_6pat_cell_annotations_June2020.rds'
+    new_batch_corrected_data = '/data/leslie/krc3004/RCC_Alireza_Sep2020/ccRCC_6pat_June2020.rds'
 
     new = readRDS(new_annot_file_path)
 
@@ -17,3 +18,5 @@ if __name__ == "__main__":
     annot_data = pd.DataFrame(np.array(new).T)
     annot_data.columns = column_names
     # so this contains 31 labels instead of 16
+
+    data = readRDS(new_batch_corrected_data)
