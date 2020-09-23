63942f83511be6beea7e00f854b6daafbb8072ec
Brennan Lee <brennanpl2000@gmail.com>
2020-09-22 23:28:03 -0400

    init. commit, more to come

---
+++ Step0_Data/code/new_data_load.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import pyreadr
+import rpy2.robjects as robjects
+readRDS = robjects['readRDS']
+
+new_annot_file_path = '/data/leslie/krc3004/RCC_Alireza_Sep2020/ccRCC_6pat_cell_annotations_June2020.rds'
+
+new = readRDS(new_annot_file_path)
+
+column_names = new.colnames
+
+annot_data = pd.DataFrame(np.array(new).T)
+annot_data.columns = column_names
