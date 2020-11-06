3b19389bfadda600583863fd81570219fa10d382
Brennan Lee <brennanpl2000@gmail.com>
2020-11-06 07:33:54 -0700

    debugging test

--- Step2_DIVA/code/plot_umap_semi.py
+++ Step2_DIVA/code/plot_umap_semi.py
@@ -32,63 +32,117 @@ def plot_umap(train_loader, test_loader, model, batch_size, test_patient, train_
     with torch.no_grad():
         # Train
         patients_train = np.delete(patients, test_patient)
-        i = 0
-        for (xs, ys, ds) in train_loader:
-            i = i + 1
-            # To device
-            xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
+        # i = 0
+        # for (xs, ys, ds) in train_loader:
+        #     i = i + 1
+        #     # To device
+        #     xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
+        #
+        #     # use classification function to compute all predictions for each batch
+        #     zy_loc, zy_scale = model.qzy(xs)
+        #     zd_loc, zd_scale = model.qzd(xs)
+        #     zx_loc, zx_scale = model.qzx(xs)
+        #     zy_.append(np.array(zy_loc.cpu()))
+        #     zd_.append(np.array(zd_loc.cpu()))
+        #     zx_.append(np.array(zx_loc.cpu()))
+        #     actuals_d.append(np.argmax(ds,axis=1))
+        #     actuals_y.append(np.argmax(ys,axis=1))
+        #     if i == 50:
+        #        break
+        #
+        # zy = zy_[0]
+        # zd = zd_[0]
+        # zx = zx_[0]
+        # labels_y = actuals_y[0]
+        # labels_d = actuals_d[0]
+        # for i in range(1,50):
+        #     zy = np.vstack((zy, zy_[i]))
+        #     zd = np.vstack((zd, zd_[i]))
+        #     zx = np.vstack((zx, zx_[i]))
+        #     labels_y = np.hstack((labels_y, actuals_y[i]))
+        #     labels_d = np.hstack((labels_d, actuals_d[i]))
+        #
+        # zy_adata, zd_adata, zx_adata = [anndata.AnnData(_) for _ in [zy, zd, zx]]
+        #
+        # name = ['zy', 'zd', 'zx']
+        # for i, _ in enumerate([zy_adata, zd_adata, zx_adata]):
+        #     _.obs['batch'] = patients[labels_d]
+        #     _.obs['cell_type'] = cell_types[labels_y]
+        #     # save_name_pat = '_diva_new_semi_sup_train_' + name[i] + '_by_batches_heldout_pat_' + str(test_patient) + '.png'
+        #     # save_name_cell_type = '_diva_new_semi_sup_train_' + name[i] + '_by_label_heldout_pat_' + str(test_patient) + '.png'
+        #     if train_patient is not None:
+        #         save_name_pat = f"_diva_new_semi_sup_train_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+        #         save_name_cell_type = f"_diva_new_semi_sup_train_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+        #     else:
+        #         save_name_pat = f"_diva_new_semi_sup_train_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
+        #         save_name_cell_type = f"_diva_new_semi_sup_train_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
+        #
+        #
+        #     sc.pp.neighbors(_, use_rep="X", n_neighbors=15)
+        #     sc.tl.umap(_, min_dist=.3)
+        #     sc.pl.umap(_, color='batch', size=15, alpha=.8, save=save_name_pat)
+        #     sc.pl.umap(_, color='cell_type', size=15, alpha=.8, save=save_name_cell_type)
+        #
+        #
+        # ## Test
+        #
+        # actuals_d, actuals_y, zy_, zd_, zx_ = [], [], [], [], []
+        # i = 0
+        # for (xs, ys, ds) in test_loader:
+        #     i = i + 1
+        #     # To device
+        #     xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
+        #
+        #     # use classification function to compute all predictions for each batch
+        #     zy_loc, zy_scale = model.qzy(xs)
+        #     zd_loc, zd_scale = model.qzd(xs)
+        #     zx_loc, zx_scale = model.qzx(xs)
+        #     zy_.append(np.array(zy_loc.cpu()))
+        #     zd_.append(np.array(zd_loc.cpu()))
+        #     zx_.append(np.array(zx_loc.cpu()))
+        #     actuals_d.append(np.argmax(ds, axis=1))
+        #     actuals_y.append(np.argmax(ys, axis=1))
+        #     if i == 50:
+        #         break
+        #
+        # zy = zy_[0]
+        # zd = zd_[0]
+        # zx = zx_[0]
+        # labels_y = actuals_y[0]
+        # labels_d = actuals_d[0]
+        # for i in range(1, 50):
+        #     zy = np.vstack((zy, zy_[i]))
+        #     zd = np.vstack((zd, zd_[i]))
+        #     zx = np.vstack((zx, zx_[i]))
+        #     labels_y = np.hstack((labels_y, actuals_y[i]))
+        #     labels_d = np.hstack((labels_d, actuals_d[i]))
+        #
+        # zy_adata, zd_adata, zx_adata = [anndata.AnnData(_) for _ in [zy, zd, zx]]
+        #
+        # name = ['zy', 'zd', 'zx']
+        # for i, _ in enumerate([zy_adata, zd_adata, zx_adata]):
+        #     _.obs['batch'] = patients[labels_d]
+        #     _.obs['cell_type'] = cell_types[labels_y]
+        #     # save_name_pat = '_diva_new_semi_sup_train_' + name[i] + '_by_batches_heldout_pat_' + str(test_patient) + '.png'
+        #     # save_name_cell_type = '_diva_new_semi_sup_train_' + name[i] + '_by_label_heldout_pat_' + str(test_patient) + '.png'
+        #     if train_patient is not None:
+        #         save_name_pat = f"_diva_new_semi_sup_test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+        #         save_name_cell_type = f"_diva_new_semi_sup_test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+        #     else:
+        #         save_name_pat = f"_diva_new_semi_sup_test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
+        #         save_name_cell_type = f"_diva_new_semi_sup_test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
+        #
+        #     sc.pp.neighbors(_, use_rep="X", n_neighbors=15)
+        #     sc.tl.umap(_, min_dist=.3)
+        #     sc.pl.umap(_, color='batch', size=15, alpha=.8, save=save_name_pat)
+        #     sc.pl.umap(_, color='cell_type', size=15, alpha=.8, save=save_name_cell_type)
 
-            # use classification function to compute all predictions for each batch
-            zy_loc, zy_scale = model.qzy(xs)
-            zd_loc, zd_scale = model.qzd(xs)
-            zx_loc, zx_scale = model.qzx(xs)
-            zy_.append(np.array(zy_loc.cpu()))
-            zd_.append(np.array(zd_loc.cpu()))
-            zx_.append(np.array(zx_loc.cpu()))
-            actuals_d.append(np.argmax(ds,axis=1))
-            actuals_y.append(np.argmax(ys,axis=1))
-            if i == 50:
-               break
+        ## Train + Test
 
-        zy = zy_[0]
-        zd = zd_[0]
-        zx = zx_[0]
-        labels_y = actuals_y[0]
-        labels_d = actuals_d[0]
-        for i in range(1,50):
-            zy = np.vstack((zy, zy_[i]))
-            zd = np.vstack((zd, zd_[i]))
-            zx = np.vstack((zx, zx_[i]))
-            labels_y = np.hstack((labels_y, actuals_y[i]))
-            labels_d = np.hstack((labels_d, actuals_d[i]))
-
-        zy_adata, zd_adata, zx_adata = [anndata.AnnData(_) for _ in [zy, zd, zx]]
-
-        name = ['zy', 'zd', 'zx']
-        for i, _ in enumerate([zy_adata, zd_adata, zx_adata]):
-            _.obs['batch'] = patients[labels_d]
-            _.obs['cell_type'] = cell_types[labels_y]
-            # save_name_pat = '_diva_new_semi_sup_train_' + name[i] + '_by_batches_heldout_pat_' + str(test_patient) + '.png'
-            # save_name_cell_type = '_diva_new_semi_sup_train_' + name[i] + '_by_label_heldout_pat_' + str(test_patient) + '.png'
-            if train_patient is not None:
-                save_name_pat = f"_diva_new_semi_sup_train_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
-                save_name_cell_type = f"_diva_new_semi_sup_train_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
-            else:
-                save_name_pat = f"_diva_new_semi_sup_train_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
-                save_name_cell_type = f"_diva_new_semi_sup_train_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
-
-
-            sc.pp.neighbors(_, use_rep="X", n_neighbors=15)
-            sc.tl.umap(_, min_dist=.3)
-            sc.pl.umap(_, color='batch', size=15, alpha=.8, save=save_name_pat)
-            sc.pl.umap(_, color='cell_type', size=15, alpha=.8, save=save_name_cell_type)
-
-
-        ## Test
-
+        patients = np.append(patients_train, patients[test_patient])
         actuals_d, actuals_y, zy_, zd_, zx_ = [], [], [], [], []
         i = 0
-        for (xs, ys, ds) in test_loader:
+        for (xs, ys, ds) in train_loader:
             i = i + 1
             # To device
             xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
@@ -105,12 +159,30 @@ def plot_umap(train_loader, test_loader, model, batch_size, test_patient, train_
             if i == 50:
                 break
 
+        i = 0
+        for (xs, ys, ds) in test_loader:
+            i = i + 1
+            # To device
+            xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
+
+            # use classification function to compute all predictions for each batch
+            zy_loc, zy_scale = model.qzy(xs)
+            zd_loc, zd_scale = model.qzd(xs)
+            zx_loc, zx_scale = model.qzx(xs)
+            zy_.append(np.array(zy_loc.cpu()))
+            zd_.append(np.array(zd_loc.cpu()))
+            zx_.append(np.array(zx_loc.cpu()))
+            actuals_d.append(np.argmax(ds, axis=1))
+            actuals_y.append(np.argmax(ys, axis=1))
+            if i == 10:
+                break
+
         zy = zy_[0]
         zd = zd_[0]
         zx = zx_[0]
         labels_y = actuals_y[0]
         labels_d = actuals_d[0]
-        for i in range(1, 50):
+        for i in range(1, 50 + 10):
             zy = np.vstack((zy, zy_[i]))
             zd = np.vstack((zd, zd_[i]))
             zx = np.vstack((zx, zx_[i]))
@@ -126,88 +198,16 @@ def plot_umap(train_loader, test_loader, model, batch_size, test_patient, train_
             # save_name_pat = '_diva_new_semi_sup_train_' + name[i] + '_by_batches_heldout_pat_' + str(test_patient) + '.png'
             # save_name_cell_type = '_diva_new_semi_sup_train_' + name[i] + '_by_label_heldout_pat_' + str(test_patient) + '.png'
             if train_patient is not None:
-                save_name_pat = f"_diva_new_semi_sup_test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
-                save_name_cell_type = f"_diva_new_semi_sup_test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+                save_name_pat = f"_diva_new_semi_sup_train+test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
+                save_name_cell_type = f"_diva_new_semi_sup_train+test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
             else:
-                save_name_pat = f"_diva_new_semi_sup_test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
-                save_name_cell_type = f"_diva_new_semi_sup_test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
+                save_name_pat = f"_diva_new_semi_sup_train+test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
+                save_name_cell_type = f"_diva_new_semi_sup_train+test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
 
             sc.pp.neighbors(_, use_rep="X", n_neighbors=15)
             sc.tl.umap(_, min_dist=.3)
             sc.pl.umap(_, color='batch', size=15, alpha=.8, save=save_name_pat)
             sc.pl.umap(_, color='cell_type', size=15, alpha=.8, save=save_name_cell_type)
-
-            ## Train + Test
-
-            patients = np.append(patients_train, patients[test_patient])
-            actuals_d, actuals_y, zy_, zd_, zx_ = [], [], [], [], []
-            i = 0
-            for (xs, ys, ds) in train_loader:
-                i = i + 1
-                # To device
-                xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
-
-                # use classification function to compute all predictions for each batch
-                zy_loc, zy_scale = model.qzy(xs)
-                zd_loc, zd_scale = model.qzd(xs)
-                zx_loc, zx_scale = model.qzx(xs)
-                zy_.append(np.array(zy_loc.cpu()))
-                zd_.append(np.array(zd_loc.cpu()))
-                zx_.append(np.array(zx_loc.cpu()))
-                actuals_d.append(np.argmax(ds, axis=1))
-                actuals_y.append(np.argmax(ys, axis=1))
-                if i == 50:
-                    break
-
-            i = 0
-            for (xs, ys, ds) in test_loader:
-                i = i + 1
-                # To device
-                xs, ys, ds = xs.to(device), np.array(ys), np.array(ds)
-
-                # use classification function to compute all predictions for each batch
-                zy_loc, zy_scale = model.qzy(xs)
-                zd_loc, zd_scale = model.qzd(xs)
-                zx_loc, zx_scale = model.qzx(xs)
-                zy_.append(np.array(zy_loc.cpu()))
-                zd_.append(np.array(zd_loc.cpu()))
-                zx_.append(np.array(zx_loc.cpu()))
-                actuals_d.append(np.argmax(ds, axis=1))
-                actuals_y.append(np.argmax(ys, axis=1))
-                if i == 10:
-                    break
-
-            zy = zy_[0]
-            zd = zd_[0]
-            zx = zx_[0]
-            labels_y = actuals_y[0]
-            labels_d = actuals_d[0]
-            for i in range(1, 50 + 10):
-                zy = np.vstack((zy, zy_[i]))
-                zd = np.vstack((zd, zd_[i]))
-                zx = np.vstack((zx, zx_[i]))
-                labels_y = np.hstack((labels_y, actuals_y[i]))
-                labels_d = np.hstack((labels_d, actuals_d[i]))
-
-            zy_adata, zd_adata, zx_adata = [anndata.AnnData(_) for _ in [zy, zd, zx]]
-
-            name = ['zy', 'zd', 'zx']
-            for i, _ in enumerate([zy_adata, zd_adata, zx_adata]):
-                _.obs['batch'] = patients[labels_d]
-                _.obs['cell_type'] = cell_types[labels_y]
-                # save_name_pat = '_diva_new_semi_sup_train_' + name[i] + '_by_batches_heldout_pat_' + str(test_patient) + '.png'
-                # save_name_cell_type = '_diva_new_semi_sup_train_' + name[i] + '_by_label_heldout_pat_' + str(test_patient) + '.png'
-                if train_patient is not None:
-                    save_name_pat = f"_diva_new_semi_sup_train+test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
-                    save_name_cell_type = f"_diva_new_semi_sup_train+test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_{train_patient}.png"
-                else:
-                    save_name_pat = f"_diva_new_semi_sup_train+test_{name[i]}_by_batches_heldout_pat_{test_patient}_train_pat_ALL.png"
-                    save_name_cell_type = f"_diva_new_semi_sup_train+test_{name[i]}_by_label_heldout_pat_{test_patient}_train_pat_ALL.png"
-
-                sc.pp.neighbors(_, use_rep="X", n_neighbors=15)
-                sc.tl.umap(_, min_dist=.3)
-                sc.pl.umap(_, color='batch', size=15, alpha=.8, save=save_name_pat)
-                sc.pl.umap(_, color='cell_type', size=15, alpha=.8, save=save_name_cell_type)
 
 
 if __name__ == "__main__":
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     for test_patient in range(6):
         if test_patient == train_patient:
             continue
-            
+
         # model_name = './' + 'rcc_new_test_domain_' + str(test_patient) + '_semi_sup_seed_' + str(seed)
         model_name = f"./rcc_new_test_domain_{test_patient}_train_domain_{train_patient}_semi_sup_seed_{seed}"
 
