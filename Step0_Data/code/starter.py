4ac4d748b489326bdf12d7ed6c34bdf27936ce3a
Brennan Lee <brennanpl2000@gmail.com>
2020-11-12 10:06:31 -0500

    init commit of python file that contains any code that could be useful in the logistic tasks pertaining to the project

---
+++ Step0_Data/code/starter.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from inspect import getsource
+
+
+def get_valid_diva_models(dir="./"):
+    """
+    Collects the names of DIVA models in `dir`
+
+    Parameters
+    ----------
+    dir : str
+        String pertaining to valid file path
+
+    Returns
+    -------
+    lst
+        list of model names without file extensions,
+        for which there exist "<name>.model" and "<name>.config" within `dir`
+
+    """
+    files = os.listdir(dir)
+    model_file_exts = set(("model", "config"))
+    name_exts = [f.split(".") for f in files if f.split(".")[-1] in model_file_exts]
+    model_names = []
+    while name_exts:
+        curr = name_exts.pop()
+        for i, f in enumerate(name_exts):
+            # if the model names are the same
+            if f[0] == curr[0]:
+                # if there is both the ".model" and ".config" extension
+                if set((f[-1], curr[-1])) == model_file_exts:
+                    model_names.append(curr[0])
+    return model_names
