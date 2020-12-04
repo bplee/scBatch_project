62ae687b01c6620a443579b9b773e0918031c5a2
Brennan Lee <brennanpl2000@gmail.com>
2020-12-04 17:16:39 -0500

    init commit

---
+++ francisco_dataset/code/data_load.py
@@ -0,0 +1,61 @@
+import time
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+WORKING_DIR = "/data/leslie/bplee/scBatch"
+# adding the project dir to the path to import relevant modules below
+if WORKING_DIR not in sys.path:
+    print("CHANGING PATH")
+    sys.path.append(WORKING_DIR)
+    print("\tWorking dir appended to Sys path.")
+
+
+def get_pat_id_from_filepath(f):
+    return os.path.split(f)[-1].split("_")[0]
+
+def read_data(f):
+    """
+
+    Parameters
+    ----------
+    f : str
+        filepath to francisco data file
+
+    Returns
+    -------
+    pandas df
+        contains cluster information, and column for patient id is put in
+
+    """
+    rtn = pd.read_csv(f, index_col=0)
+    rtn["PATIENT"] = get_pat_id_from_filepath(f)
+    return rtn
+
+def concat_data(directory="/data/leslie/bplee/scBatch/francisco_dataset/data/"):
+    """
+
+    Parameters
+    ----------
+    directory : str
+        directory where francisco's data is
+
+    Returns
+    -------
+    pandas df
+        contains all counts and patient and cluster columms
+    """
+    for i, f in enumerate(os.listdir(directory)):
+        df = read_data(f)
+        if i == 0:
+            rtn = df
+        else:
+            rtn = pd.concat([rtn, df], axis=0)
+    return rtn
+
+if __name__ == "__main__":
+
+    all_data = concat_data()
+
+
