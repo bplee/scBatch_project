1de7be7175890649960f6ab437c8680bce135c6c
Brennan Lee <brennanpl2000@gmail.com>
2021-02-25 10:39:17 -0500

    adding in encoding dim arg into argparser

--- Step6_RCC_to_CRC/code/rcc_to_crc_diva.py
+++ Step6_RCC_to_CRC/code/rcc_to_crc_diva.py
@@ -210,6 +210,8 @@ if __name__ == "__main__":
                         help='size of latent space 2')
     parser.add_argument('--zy-dim', type=int, default=64,
                         help='size of latent space 3')
+    parser.add_argument('--encoding-dim', type=int, default=1024,
+                        help='dimension encoding layers work down to')
 
     # Aux multipliers
     parser.add_argument('--aux_loss_multiplier_y', type=float, default=4200.,
