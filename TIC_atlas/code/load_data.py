00e00cf68b6c4ab8aa145351cbbe8251c17db744
Brennan Lee <brennanpl2000@gmail.com>
2021-03-23 12:41:10 -0400

    setting up the loading of data

--- TIC_atlas/code/load_data.py
+++ TIC_atlas/code/load_data.py
@@ -2,7 +2,9 @@ import anndata
 import sys
 import os
 import scanpy as sc
+import numpy as np
 import pandas as pd
+from scvi.dataset import GeneExpressionDataset
 
 WORKING_DIR = "/data/leslie/bplee/scBatch"
 # adding the project dir to the path to import relevant modules below
@@ -19,20 +21,71 @@ TIC_META_DATA_PATH = "/data/leslie/bplee/scBatch/TIC_atlas/data/TICAtlas_metadat
 TIC_DOWNSAMPLED_PATH = "/data/leslie/bplee/scBatch/TIC_atlas/data/TICAtlas_downsampled_1000.h5ad"
 
 def load_data(data_path=TIC_DATA_PATH):
+    """
+    loads TIC atlas data [317111 x 87659]
+
+    Parameters
+    ----------
+    data_path
+
+    Returns
+    -------
+    anndata.Dataframe
+        has sparse matrix in the count part
+        has a 'percent.mt' column
+        gender, source (where the sample comes from), subtype (tumor type)
+    """
     return anndata.read_h5ad(data_path)
 
 def load_meta_data(meta_data_path=TIC_META_DATA_PATH):
     return pd.read_csv(meta_data_path, index_col=0)
 
+
 def load_downsampled(data_path=TIC_DOWNSAMPLED_PATH):
     return anndata.read_h5ad(data_path)
 
-def remove_low_count_patients(adata):
-
 
 def clean_tic(adata):
+    """
+
+    Parameters
+    ----------
+    adata : anndata.AnnData
+        input from load adata
+
+    Returns
+    -------
+    anndata.AnnData
+
+    """
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    adata.var['ribo'] = adata.var_names.str.startswith(("RP"))
+    # no mt genes in this data, but they have percent mt from prior analysis
+    print(f" Number of MT genes: {sum(adata.var['mt'])} / {adata.shape[1]}")
+    print(f" Number of Ribo genes: {sum(adata.var['ribo'])} / {adata.shape[1]}")
+    print(f" Removing ribo and mitochondrial genes")
+
+    print(f" Number of cells with MT%>20: {sum(adata.obs['percent.mt'] > 20)} ")
+    adata = adata[adata.obs['percent.mt' < 20, :]]
+
+    extra_removing = ["NEAT1", "MALAT1"]
+    print(f" Removing {extra_removing} from list")
+    extra_bools = adata.var_names.isin(extra_removing)  # these are true for NEAT1 and MALAT1
+    keep_genes = ~adata.var.mt & ~adata.var.ribo & ~extra_bools
+
+    adata = adata[:, keep_genes]
 
     return adata
 
 if __name__ == "__main__":
     adata = load_data()
+    adata = clean_tic(adata)
+    gene_ds = GeneExpressionDataset()
+    tumor_types = adata.obs.subtype
+    gene_ds.populate_from_data(X=adata.X,
+                               gene_names=np.array(adata.var.index),
+                               batch_indices=pd.factorize(tumor_types)[0],
+                               remap_attributes=False)
+    gene_ds.subsample_genes(784)
+
+    adata = adata[:, gene_ds.gene_names]
