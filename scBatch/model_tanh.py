e65370494ae1872b0497db1b42548cdbce67c99a
Brennan Lee <brennanpl2000@gmail.com>
2021-05-19 15:34:42 -0400

    model_tanh.py init commit

---
+++ scBatch/model_tanh.py
@@ -0,0 +1,429 @@
+"""
+DIVA model architecture for 2 linear layers
+"""
+print("Loaded 2layer DIVA encoding model")
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.distributions as dist
+import numpy as np
+
+### Follows model as seen in LEARNING ROBUST REPRESENTATIONS BY PROJECTING SUPERFICIAL STATISTICS OUT
+
+
+# Encoders
+class qzd(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim, encoding_dim):
+        super(qzd, self).__init__()
+        self.encoding_dim = encoding_dim
+
+        self.encoder = nn.Sequential(
+            nn.Linear(x_dim, encoding_dim), nn.Tanh(),
+            nn.Linear(encoding_dim, 384), nn.Tanh()
+        )
+
+        self.fc11 = nn.Sequential(nn.Linear(384, zd_dim))
+        self.fc12 = nn.Sequential(nn.Linear(384, zd_dim), nn.Softplus())
+
+        torch.nn.init.xavier_uniform_(self.encoder[0].weight)
+        torch.nn.init.xavier_uniform_(self.fc11[0].weight)
+        self.fc11[0].bias.data.zero_()
+        torch.nn.init.xavier_uniform_(self.fc12[0].weight)
+        self.fc12[0].bias.data.zero_()
+
+    def forward(self, x):
+        h = self.encoder(x)
+        h = h.view(-1, 384)
+        zd_loc = self.fc11(h)
+        zd_scale = self.fc12(h) + 1e-7
+
+        return zd_loc, zd_scale
+
+
+class qzx(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim, encoding_dim):
+        super(qzx, self).__init__()
+        self.encoding_dim = encoding_dim
+
+        self.encoder = nn.Sequential(
+            nn.Linear(x_dim, encoding_dim), nn.Tanh(),
+            nn.Linear(encoding_dim, 384), nn.Tanh()
+        )
+
+        self.fc11 = nn.Sequential(nn.Linear(384, zx_dim))
+        self.fc12 = nn.Sequential(nn.Linear(384, zx_dim), nn.Softplus())
+
+        torch.nn.init.xavier_uniform_(self.encoder[0].weight)
+        torch.nn.init.xavier_uniform_(self.fc11[0].weight)
+        self.fc11[0].bias.data.zero_()
+        torch.nn.init.xavier_uniform_(self.fc12[0].weight)
+        self.fc12[0].bias.data.zero_()
+
+    def forward(self, x):
+        h = self.encoder(x)
+        h = h.view(-1, 384)
+        zx_loc = self.fc11(h)
+        zx_scale = self.fc12(h) + 1e-7
+
+        return zx_loc, zx_scale
+
+
+class qzy(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim, encoding_dim):
+        super(qzy, self).__init__()
+        self.encoding_dim = encoding_dim
+
+        self.encoder = nn.Sequential(
+            nn.Linear(x_dim, encoding_dim), nn.ReLU(),
+            nn.Linear(encoding_dim, 384), nn.ReLU()
+        )
+
+        self.fc11 = nn.Sequential(nn.Linear(encoding_dim, zy_dim))
+        self.fc12 = nn.Sequential(nn.Linear(encoding_dim, zy_dim), nn.Softplus())
+
+        torch.nn.init.xavier_uniform_(self.encoder[0].weight)
+        torch.nn.init.xavier_uniform_(self.fc11[0].weight)
+        self.fc11[0].bias.data.zero_()
+        torch.nn.init.xavier_uniform_(self.fc12[0].weight)
+        self.fc12[0].bias.data.zero_()
+
+    def forward(self, x):
+        h = self.encoder(x)
+        h = h.view(-1, 384)
+        zy_loc = self.fc11(h)
+        zy_scale = self.fc12(h) + 1e-7
+
+        return zy_loc, zy_scale
+
+# Decoders
+class px(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim, encoding_dim):
+        super(px, self).__init__()
+
+        self.fc1 = nn.Sequential(nn.Linear(zd_dim + zx_dim + zy_dim, 384, bias=False), nn.ReLU())
+        self.fc2 = nn.Sequential(nn.Linear(384, encoding_dim, bias=False), nn.ReLU())
+        self.fc3 = nn.Sequential(nn.Linear(encoding_dim, x_dim, bias=False), nn.ReLU())
+
+        torch.nn.init.xavier_uniform_(self.fc1[0].weight)
+
+    def forward(self, zd, zx, zy):
+        if zx is None:
+            # if no zx layer
+            zdzxzy = torch.cat((zd, zy), dim=-1)
+        else:
+            zdzxzy = torch.cat((zd, zx, zy), dim=-1)
+        h = self.fc1(zdzxzy)
+        h = self.fc2(h)
+        h = self.fc3(h)
+        return h
+
+
+class pzd(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim):
+        super(pzd, self).__init__()
+        self.fc1 = nn.Sequential(nn.Linear(d_dim, zd_dim, bias=False), nn.ReLU())
+
+        self.fc21 = nn.Sequential(nn.Linear(zd_dim, zd_dim))
+        self.fc22 = nn.Sequential(nn.Linear(zd_dim, zd_dim), nn.Softplus())
+
+        torch.nn.init.xavier_uniform_(self.fc1[0].weight)
+        torch.nn.init.xavier_uniform_(self.fc21[0].weight)
+        self.fc21[0].bias.data.zero_()
+        torch.nn.init.xavier_uniform_(self.fc22[0].weight)
+        self.fc22[0].bias.data.zero_()
+
+    def forward(self, d):
+        hidden = self.fc1(d)
+        zd_loc = self.fc21(hidden)
+        zd_scale = self.fc22(hidden) + 1e-7
+
+        return zd_loc, zd_scale
+
+
+class pzy(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim):
+        super(pzy, self).__init__()
+        self.fc1 = nn.Sequential(nn.Linear(y_dim, zy_dim, bias=False), nn.ReLU())
+        self.fc21 = nn.Sequential(nn.Linear(zy_dim, zy_dim))
+        self.fc22 = nn.Sequential(nn.Linear(zy_dim, zy_dim), nn.Softplus())
+
+        torch.nn.init.xavier_uniform_(self.fc1[0].weight)
+        torch.nn.init.xavier_uniform_(self.fc21[0].weight)
+        self.fc21[0].bias.data.zero_()
+        torch.nn.init.xavier_uniform_(self.fc22[0].weight)
+        self.fc22[0].bias.data.zero_()
+
+    def forward(self, y):
+        hidden = self.fc1(y)
+        zy_loc = self.fc21(hidden)
+        zy_scale = self.fc22(hidden) + 1e-7
+
+        return zy_loc, zy_scale
+
+
+# Auxiliary tasks
+class qd(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim):
+        super(qd, self).__init__()
+
+        self.fc1 = nn.Linear(zd_dim, d_dim)
+
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.fc1.bias.data.zero_()
+
+    def forward(self, zd):
+        h = F.relu(zd)
+        loc_d = self.fc1(h)
+
+        return loc_d
+
+
+class qy(nn.Module):
+    def __init__(self, d_dim, x_dim, y_dim, zd_dim, zx_dim, zy_dim):
+        super(qy, self).__init__()
+
+        self.fc1 = nn.Linear(zy_dim, y_dim)
+
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.fc1.bias.data.zero_()
+
+    def forward(self, zy):
+        h = F.relu(zy)
+        loc_y = self.fc1(h)
+
+        return loc_y
+
+
+class DIVA(nn.Module):
+    def __init__(self, args):
+        super(DIVA, self).__init__()
+        self.zd_dim = args.zd_dim
+        self.zx_dim = args.zx_dim
+        self.zy_dim = args.zy_dim
+        self.d_dim = args.d_dim
+        self.x_dim = args.x_dim
+        self.y_dim = args.y_dim
+        self.encoding_dim = args.encoding_dim
+
+        self.start_zx = self.zd_dim
+        self.start_zy = self.zd_dim + self.zx_dim
+
+        self.px = px(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim, self.encoding_dim)
+        self.pzd = pzd(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim)
+        self.pzy = pzy(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim)
+
+        self.qzd = qzd(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim, self.encoding_dim)
+        if self.zx_dim != 0:
+            self.qzx = qzx(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim, self.encoding_dim)
+        self.qzy = qzy(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim, self.encoding_dim)
+
+        self.qd = qd(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim)
+        self.qy = qy(self.d_dim, self.x_dim, self.y_dim, self.zd_dim, self.zx_dim, self.zy_dim)
+
+        self.aux_loss_multiplier_y = args.aux_loss_multiplier_y
+        self.aux_loss_multiplier_d = args.aux_loss_multiplier_d
+
+        self.beta_d = args.beta_d
+        self.beta_x = args.beta_x
+        self.beta_y = args.beta_y
+
+        self.cuda()
+
+    def forward(self, d, x, y):
+        # Encode
+        zd_q_loc, zd_q_scale = self.qzd(x)
+        if self.zx_dim != 0:
+            zx_q_loc, zx_q_scale = self.qzx(x)
+        zy_q_loc, zy_q_scale = self.qzy(x)
+
+        # Reparameterization trick
+        qzd = dist.Normal(zd_q_loc, zd_q_scale)
+        zd_q = qzd.rsample()
+        if self.zx_dim != 0:
+            qzx = dist.Normal(zx_q_loc, zx_q_scale)
+            zx_q = qzx.rsample()
+        else:
+            qzx = None
+            zx_q = None
+
+        qzy = dist.Normal(zy_q_loc, zy_q_scale)
+        zy_q = qzy.rsample()
+
+        # Decode
+        x_recon = self.px(zd_q, zx_q, zy_q)
+
+        zd_p_loc, zd_p_scale = self.pzd(d)
+
+        if self.zx_dim != 0:
+            zx_p_loc, zx_p_scale = torch.zeros(zd_p_loc.size()[0], self.zx_dim).cuda(),\
+                                   torch.ones(zd_p_loc.size()[0], self.zx_dim).cuda()
+        zy_p_loc, zy_p_scale = self.pzy(y) # threw an error for float vs. long here
+
+        # Reparameterization trick
+        pzd = dist.Normal(zd_p_loc, zd_p_scale)
+        if self.zx_dim != 0:
+            pzx = dist.Normal(zx_p_loc, zx_p_scale)
+        else:
+            pzx = None
+        pzy = dist.Normal(zy_p_loc, zy_p_scale)
+
+        # Auxiliary losses
+        d_hat = self.qd(zd_q)
+        y_hat = self.qy(zy_q)
+
+        return x_recon, d_hat, y_hat, qzd, pzd, zd_q, qzx, pzx, zx_q, qzy, pzy, zy_q
+
+    def loss_function(self, d, x, y=None):
+        if y is None:  # unsupervised
+            # Do standard forward pass for everything not involving y
+            zd_q_loc, zd_q_scale = self.qzd(x)
+            if self.zx_dim != 0:
+                zx_q_loc, zx_q_scale = self.qzx(x)
+            zy_q_loc, zy_q_scale = self.qzy(x)
+
+            qzd = dist.Normal(zd_q_loc, zd_q_scale)
+            zd_q = qzd.rsample()
+            if self.zx_dim != 0:
+                qzx = dist.Normal(zx_q_loc, zx_q_scale)
+                zx_q = qzx.rsample()
+            else:
+                zx_q = None
+            qzy = dist.Normal(zy_q_loc, zy_q_scale)
+            zy_q = qzy.rsample()
+
+            zd_p_loc, zd_p_scale = self.pzd(d)
+            if self.zx_dim != 0:
+                zx_p_loc, zx_p_scale = torch.zeros(zd_p_loc.size()[0], self.zx_dim).cuda(), \
+                                       torch.ones(zd_p_loc.size()[0], self.zx_dim).cuda()
+
+            pzd = dist.Normal(zd_p_loc, zd_p_scale)
+
+            if self.zx_dim != 0:
+                pzx = dist.Normal(zx_p_loc, zx_p_scale)
+            else:
+                pzx = None
+
+            d_hat = self.qd(zd_q)
+
+            x_recon = self.px(zd_q, zx_q, zy_q)
+
+
+            x_target = x
+            # CE_x = F.cross_entropy(x_recon, x_target, reduction='sum')
+            # using reconstruction instead
+            CE_x = torch.norm(x_recon - x_target)
+
+            zd_p_minus_zd_q = torch.sum(pzd.log_prob(zd_q) - qzd.log_prob(zd_q))
+            if self.zx_dim != 0:
+                KL_zx = torch.sum(pzx.log_prob(zx_q) - qzx.log_prob(zx_q))
+            else:
+                KL_zx = 0
+
+            _, d_target = d.max(dim=1)
+            CE_d = F.cross_entropy(d_hat, d_target, reduction='sum')
+            # not using reconstruction here since labels
+            # CE_d = torch.norm(d_hat, d_target)
+
+            num_labels = self.y_dim
+            # num_labels = y.shape[1]
+
+            # Create labels and repeats of zy_q and qzy
+            y_onehot = torch.eye(num_labels)
+            y_onehot = y_onehot.repeat(1, x.shape[0])
+            y_onehot = y_onehot.view(num_labels * x.shape[0], num_labels).cuda()
+
+            zy_q = zy_q.repeat(num_labels, 1)
+            zy_q_loc, zy_q_scale = zy_q_loc.repeat(num_labels, 1), zy_q_scale.repeat(num_labels, 1)
+            qzy = dist.Normal(zy_q_loc, zy_q_scale)
+
+            # Do forward pass for everything involving y
+            zy_p_loc, zy_p_scale = self.pzy(y_onehot)
+
+            # Reparameterization trick
+            pzy = dist.Normal(zy_p_loc, zy_p_scale)
+
+            # Auxiliary losses
+            y_hat = self.qy(zy_q)
+
+            # Marginals
+            alpha_y = F.softmax(y_hat, dim=-1)
+            qy = dist.OneHotCategorical(alpha_y)
+            prob_qy = torch.exp(qy.log_prob(y_onehot))
+
+            zy_p_minus_zy_q = torch.sum(pzy.log_prob(zy_q) - qzy.log_prob(zy_q), dim=-1)
+
+            marginal_zy_p_minus_zy_q = torch.sum(prob_qy * zy_p_minus_zy_q)
+
+            prior_y = torch.tensor(1/num_labels).cuda()
+            prior_y_minus_qy = torch.log(prior_y) - qy.log_prob(y_onehot)
+            marginal_prior_y_minus_qy = torch.sum(prob_qy * prior_y_minus_qy)
+
+            return CE_x \
+                   - self.beta_d * zd_p_minus_zd_q \
+                   - self.beta_x * KL_zx \
+                   - self.beta_y * marginal_zy_p_minus_zy_q \
+                   - marginal_prior_y_minus_qy \
+                   + self.aux_loss_multiplier_d * CE_d
+
+        else: # supervised
+            x_recon, d_hat, y_hat, qzd, pzd, zd_q, qzx, pzx, zx_q, qzy, pzy, zy_q = self.forward(d, x, y)
+
+            x_target = x
+            # mse of reconstructionx
+            CE_x = torch.norm(x_recon - x_target)
+
+            zd_p_minus_zd_q = torch.sum(pzd.log_prob(zd_q) - qzd.log_prob(zd_q))
+            if self.zx_dim != 0:
+                KL_zx = torch.sum(pzx.log_prob(zx_q) - qzx.log_prob(zx_q))
+            else:
+                KL_zx = 0
+
+            zy_p_minus_zy_q = torch.sum(pzy.log_prob(zy_q) - qzy.log_prob(zy_q))
+
+            _, d_target = d.max(dim=1)
+            CE_d = F.cross_entropy(d_hat, d_target, reduction='sum')
+
+            _, y_target = y.max(dim=1)
+            CE_y = F.cross_entropy(y_hat, y_target, reduction='sum')
+
+            return CE_x \
+                   - self.beta_d * zd_p_minus_zd_q \
+                   - self.beta_x * KL_zx \
+                   - self.beta_y * zy_p_minus_zy_q \
+                   + self.aux_loss_multiplier_d * CE_d \
+                   + self.aux_loss_multiplier_y * CE_y,\
+                   CE_y
+
+    def classifier(self, x):
+        """
+        classify an image (or a batch of images)
+        :param xs: a batch of scaled vectors of pixels from an image
+        :return: a batch of the corresponding class labels (as one-hots)
+        """
+        with torch.no_grad():
+            zd_q_loc, zd_q_scale = self.qzd(x)
+            zd = zd_q_loc
+            alpha = F.softmax(self.qd(zd), dim=1)
+
+            # get the index (digit) that corresponds to
+            # the maximum predicted class probability
+            res, ind = torch.topk(alpha, 1)
+
+            # convert the digit(s) to one-hot tensor(s)
+            d = x.new_zeros(alpha.size())
+            d = d.scatter_(1, ind, 1.0)
+
+            zy_q_loc, zy_q_scale = self.qzy.forward(x)
+            zy = zy_q_loc
+            alpha = F.softmax(self.qy(zy), dim=1)
+
+            # get the index (digit) that corresponds to
+            # the maximum predicted class probability
+            res, ind = torch.topk(alpha, 1)
+
+            # convert the digit(s) to one-hot tensor(s)
+            y = x.new_zeros(alpha.size())
+            y = y.scatter_(1, ind, 1.0)
+
+        return d, y
