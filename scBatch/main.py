0e2822aa05c382fca7631e1456c1e394c3597d95
Brennan Lee <brennanpl2000@gmail.com>
2021-05-20 17:37:26 -0400

    main.py: pep8 fixes, added a repr, altered model name and fig name issue

--- scBatch/main.py
+++ scBatch/main.py
@@ -14,33 +14,43 @@ from .model_2layers import DIVA
 import numpy as np
 import torch.utils.data as data_utils
 
+
 class DIVAModel:
     def __init__(self, args=None):
-
         if args is None:
             from .args import default_args
             self.set_args(default_args)
         else:
             # allow user to input their own args file
-            self.args = args
+            self.set_args(args)
 
+        # model architecture is dependent on data so can't set it yet
         self.model = None
+        # data loaders are stored in the obj
         self.train_loader, self.test_loader, self.valid_loader = None, None, None
+        self.model_path = None
+        self.fig_name = None
 
-    def set_data_loaders(self, train, valid, test):
-        self.train_loader = train
-        self.valid_loader = valid
-        self.test_loader = test
+    def __repr__(self):
+        print(self.model)
+        print("Data")
+        if self.train_loader is not None:
+            print(f" Training set shape: {self.train_loader.shape}")
+        if self.valid_loader is not None:
+            print(f" Validation set shape: {self.valid_loader.shape}")
+        if self.test_loader is not None:
+            print(f" Testing set shape: {self.test_loader.shape}")
+
+    def set_data_loaders(self, train_loader, valid_loader, test_loader):
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.test_loader = test_loader
 
     def set_args(self, args):
         self.args = args
 
     def create_model_from_args(self):
         self.set_model(DIVA(self.args))
-
-    def create_model_from_config(self, config_filepath):
-        # TODO
-        pass
 
     def load_model_from_args(self):
         print(f" loading model from args: {self.args}")
@@ -65,11 +75,11 @@ class DIVAModel:
         print(f" changing x_dim from {self.args.x_dim} to {value}")
         self.args.x_dim = value
 
-    def set_model_name(self, name):
-        self.model_name = name
+    def set_model_path(self, name):
+        self.model_path = name
 
-    def set_fig_name(self, str):
-        self.fig_name = str
+    def set_fig_name(self, name):
+        self.fig_name = name
 
     def set_model(self, model):
         self.model = model
@@ -79,16 +89,33 @@ class DIVAModel:
         model = torch.load(name + ".model")
         self.set_model(model)
         self.set_args(args)
-        self.set_model_name(name)
+        self.set_model_path(name)
 
     @staticmethod
     def adata_to_diva_loaders(adata):
-        train_loader, test_loader = dataprep.get_diva_loaders(adata, domain_name="patient", label_name="cell_type", shuffle=True)
+        train_loader, test_loader = dataprep.get_diva_loaders(adata,
+                                                              domain_name="patient",
+                                                              label_name="cell_type",
+                                                              shuffle=True)
         new_train_loader, validation_loader = dataprep.get_validation_from_training(train_loader)
 
         return new_train_loader, validation_loader, test_loader
 
-    def fit(self, adata, epochs=200, model_name=None):
+    def fit(self, adata, model_name=None):
+        """
+
+        runs training procedure for n epochs (set in args)
+
+        Parameters
+        ----------
+        adata : anndata obj
+        model_name : str
+
+        Returns
+        -------
+        None
+
+        """
         train_loader, validation_loader, test_loader = DIVAModel.adata_to_diva_loaders(adata)
         self.set_data_loaders(train_loader, validation_loader, test_loader)
     #     self._fit_diva_loaders(train_loader, validation_loader, epochs, model_name)
@@ -101,13 +128,13 @@ class DIVAModel:
         # Model name
         print(self.args.outpath)
         if model_name is None:
-            model_name = f"{self.args.outpath}DIVAModel_seed_{self.args.seed}"
+            model_path = f"{self.args.outpath}DIVAModel_seed_{self.args.seed}"
             fig_name = f"DIVAModel_seed_{self.args.seed}"
         else:
-            model_name = f"{self.args.outpath}{model_name}_seed_{self.args.seed}"
+            model_path = f"{self.args.outpath}{model_name}_seed_{self.args.seed}"
             fig_name = f"{model_name}_seed_{self.args.seed}"
         print(model_name)
-        self.set_model_name(model_name)
+        self.set_model_path(model_path)
         self.set_fig_name(fig_name)
 
         # Set seed
@@ -140,7 +167,7 @@ class DIVAModel:
         self.set_model_x_dim(num_dims)
         self.load_model_from_args()
 
-        train.epoch_procedure(self.model_name, self.args, self.model, data_loaders, device)
+        train.epoch_procedure(self.model_path, self.args, self.model, data_loaders, device)
 
         print("Training Accuracy")
         print(train.get_accuracy(data_loaders['sup'], self.model, device))
@@ -150,35 +177,3 @@ class DIVAModel:
         print(train.get_accuracy(data_loaders['unsup'], self.model, device))
 
         visualization.plot_embeddings(self.model, data_loaders, device, self.fig_name)
-
-    def transform(self, y):
-        pass
-
-    # def fit_transform(self, d, x, y):
-    #     fit(d, x, y)
-    #     transform()
-
-
-def run_DIVA(adata, args):
-    domain_name = "patient"
-    label_name = "cell_type"
-
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if args.cuda else "cpu")
-    kwargs = {'num_workers': 1, 'pin_memory': False} if args.cuda else {}
-
-    args.d_dim = len(np.unique(adata.obs[domain_name]))
-    args.y_dim = len(np.unique(adata.obs[label_name]))
-
-    print(args.outpath)
-    model_name = f"{args.outpath}210317_rcc_to_crc_no_conv_semi_sup_seed_{args.seed}"
-    fig_name = f"210317_rcc_to_crc_no_conv_semi_sup_seed_{args.seed}"
-    print(model_name)
-
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.benchmark = False
-    np.random.seed(args.seed)
-
-    obj = DIVAModel(args=args)
-
-
