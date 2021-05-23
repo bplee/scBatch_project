8d5ace7c1b4297b516600bd1b263f0de6f2cf373
Brennan Lee <brennanpl2000@gmail.com>
2021-05-23 10:45:49 -0400

    typo in broad load_data.py

--- broad_rcc/code/load_data.py
+++ broad_rcc/code/load_data.py
@@ -15,7 +15,7 @@ if WORKING_DIR not in sys.path:
 from Step0_Data.code.starter import *
 
 COUNTS_FILEPATH = "/data/leslie/bplee/scBatch/broad_rcc/data/SCP1288/expression/ccRCC_scRNASeq_NormalizedCounts.txt"
-METADATA_FILEPATH = "/data/leslie/bplee/scBatch/broad_rcc/data/SCP1288/metadata/ccRCC_scRNASeq_NormalizedCounts.txt"
+METADATA_FILEPATH = "/data/leslie/bplee/scBatch/broad_rcc/data/SCP1288/metadata/Final_SCP_Metadata.txt"
 
 def load_data(counts_path=COUNTS_FILEPATH, metadata_path=METADATA_FILEPATH):
     adata = anndata.read_text(counts_path)
