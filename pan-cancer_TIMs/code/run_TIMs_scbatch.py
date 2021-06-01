1685ccc9ed2a031d8545c8eefea864eda6a63c97
Brennan Lee <brennanpl2000@gmail.com>
2021-06-01 11:56:06 -0400

    bug fixes and setting up run_TIMs_scbatch.py

--- pan-cancer_TIMs/code/run_TIMs_scbatch.py
+++ pan-cancer_TIMs/code/run_TIMs_scbatch.py
@@ -13,7 +13,7 @@ if WORKING_DIR not in sys.path:
     print("\tWorking dir appended to Sys path.")
 from scBatch.main import DIVAObject
 from scBatch.dataprep import set_adata_train_test_batches
-from load_data import quick_load
+from load_data import *
 
 # helper function for encoding bool into ssl arg
 def str2bool(v):
