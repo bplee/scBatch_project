91bbc3a4355b3cb6acc1c735d0185066e5430592
Brennan Lee <brennanpl2000@gmail.com>
2021-07-08 12:54:40 -0400

    bringin in quickload

--- sclc_peer/code/load_data.py
+++ sclc_peer/code/load_data.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import anndata
 import scanpy as sc
+import os
 
 
+data_dir="/lila/data/leslie/bplee/scBatch_project/sclc_peer/data/"
+quickload="/lila/data/leslie/bplee/scBatch_project/sclc_peer/quickload_data/adata_sclc_only.h5ad"
 
-data_dir="/data/leslie/bplee/scBatch_project/sclc_peer/data/"
+def quickload_sclc(filepath=quickload):
+    return anndata.read_h5ad(filepath)
